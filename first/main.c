9b0c11fde9557cd501491324d5786f3290998d0d
LeeDongHui <leedonghui@users.noreply.github.com>
2021-02-21 13:56:47 +0900

    Add project files.

---
+++ first/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h> // windows and console - system head
+
+int main(void)
+{
+	printf("Be happy!");
+	system("pause"); // keep windows(app view)
+
+	return 0;
+}
+
+// Visual studio 2019
+// All Save: Ctrl + Shift + S
+// Build Solution: Ctrl + shift + B
+// run: ctrl + F5
